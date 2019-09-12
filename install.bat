67b5bae14ec4ff13cee24bd834cd28ef60d24bd5
Christian Hansen <cwh@buildunity.dk>
2019-09-12 20:54:30 +0200

    Added installations to install.bat

--- install.bat
+++ install.bat
@@ -1,22 +1,114 @@
 @echo off
 
+goto check_Permissions
+
+:check_Permissions
+    echo Administrative permissions required. Detecting permissions...
+
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo Success: Administrative permissions confirmed.
+
+        goto chocolatey
+    ) else (
+        echo Failure: Current permissions inadequate.
+    )
+
+    pause >nul & exit
+
+:chocolatey
 WHERE choco >nul 2>nul
 IF %ERRORLEVEL% NEQ 0 (
-    if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+    echo|set /p="[INFO] Installing Chocolatey... "
     
-    @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+    @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" >nul && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+    
+    IF %ERRORLEVEL% NEQ 0 (
+        echo SUCCESS
+    ) else (
+        echo FAILED
+        pause >nul & exit
+    )
 )
 
-WHERE git >nul 2>nul
-IF %ERRORLEVEL% NEQ 0 (
-    if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
-    
-    choco install -y git --params "/WindowsTerminal /NoShellIntegration /GitOnlyOnPath /SChannel"
+:git
+IF not exist "%PROGRAMFILES%\Git\bin\bash.exe" (
+    echo|set /p="[INFO] Installing Git... "
+
+    choco install git --params "/WindowsTerminal /NoShellIntegration /GitOnlyOnPath /SChannel" >nul 2>nul
+
+    IF %ERRORLEVEL% NEQ 0 (
+        echo SUCCESS
+    ) else (
+        echo FAILED
+        pause >nul & exit
+    )
 )
 
-reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v HideFileExt /t REG_DWORD /d 0 /f
-powershell -c gps 'explorer' ^| stop-process
+:cygwin
+IF "%CYGWIN_PATH%"=="" (
+    echo|set /p="[INFO] Installing Cygwin... "
 
-rd /s /q "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\Git"
+    Call :Browse4Folder "Choose location to install Cygwin"
+)
 
-"%PROGRAMFILES%\Git\bin\bash.exe" .\install.sh
+IF "%CYGWIN_PATH%"=="" (
+    if exist "%Location%/" (
+        if not exist %Location%/bin/cygpath.exe (
+            choco install cygwin --params "/InstallDir:%Location% /NoStartMenu"
+
+            IF "%Location%" neq "Dialog Cancelled" (
+                echo SUCCESS
+
+                setx CYGWIN_PATH %Location% >nul 2>nul
+                set CYGWIN_PATH=%Location% >nul 2>nul
+                
+                goto install
+            )
+        ) else (
+            echo SUCCESS
+
+            setx CYGWIN_PATH %Location% >nul 2>nul
+            set CYGWIN_PATH=%Location% >nul 2>nul
+
+            echo [INFO] Cygwin was already installed at %Location%
+
+            goto install
+        )
+    )
+
+    echo FAILED
+    pause >nul & exit
+)
+
+:install
+SET DIR=%~dp0
+SET DIR=%DIR:~0,-1%
+
+runas /user:%userdomain%\%username% "%PROGRAMFILES%\Git\bin\bash.exe %DIR%\install.sh %*"
+exit
+
+
+::***************************************************************************
+:Browse4Folder
+set Location=
+set vbs="%temp%\_.vbs"
+set cmd="%temp%\_.cmd"
+for %%f in (%vbs% %cmd%) do if exist %%f del %%f
+for %%g in ("vbs cmd") do if defined %%g set %%g=
+(
+    echo set shell=WScript.CreateObject("Shell.Application"^) 
+    echo set f=shell.BrowseForFolder(0,"%~1",0,"%~2"^) 
+    echo if typename(f^)="Nothing" Then  
+    echo wscript.echo "set Location=Dialog Cancelled" 
+    echo WScript.Quit(1^)
+    echo end if 
+    echo set fs=f.Items(^):set fi=fs.Item(^) 
+    echo p=fi.Path:wscript.echo "set Location=" ^& p
+)>%vbs%
+cscript //nologo %vbs% > %cmd%
+for /f "delims=" %%a in (%cmd%) do %%a
+for %%f in (%vbs% %cmd%) do if exist %%f del /f /q %%f
+for %%g in ("vbs cmd") do if defined %%g set %%g=
+goto :eof
+::***************************************************************************
--- install.sh
+++ install.sh
@@ -1,5 +1,56 @@
 #!/bin/bash
 
-echo "Hello world"
+# Section: Used from https://medium.com/@Drew_Stokes/bash-argument-parsing-54f3b81a6a8f all credit goes to Drew Stokes<https://medium.com/@Drew_Stokes>
+PARAMS=""
+while (( "$#" )); do
+  case "$1" in
+    -p|--profile)
+      PROFILE=$2
+      shift 2
+      ;;
+    --) # end argument parsing
+      shift
+      break
+      ;;
+    -*|--*=) # unsupported flags
+      echo "Error: Unsupported flag $1" >&2
+      exit 1
+      ;;
+    *) # preserve positional arguments
+      PARAMS="$PARAMS $1"
+      shift
+      ;;
+  esac
+done
+# set positional arguments in their proper place
+eval set -- "${PARAMS:1}"
+unset PARAMS
+# End Section
+
+# Section: Used from https://stackoverflow.com/a/246128 all credit goes to Dave Dopson<https://stackoverflow.com/users/407731>
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+PATH="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+unset DIR
+unset SOURCE
+# End Section
+
+bool(){ return "$((!${#1}))"; }
+
+PROFILE_PATH=$PATH/profiles/$PROFILE/settings.conf
+if test -f "$PROFILE_PATH"; then
+    source $PROFILE_PATH
+    echo "[INFO] Loaded profile: $PROFILE from $PROFILE_PATH"
+fi
+
+if [[ -z $CONF_INSTALL_VSCODE ]]; then
+    echo "[INFO] Installing VSCode"
+else
+    echo "[INFO] Installing VSCode... Skipping"
+fi
 
 read
